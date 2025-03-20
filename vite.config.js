5f8ffb783e7995ccc2acb640ac296dbf29042cbe
AREENAABDULLAH <areenaabdullah120@gmail.com>
2025-03-20 14:42:59 +0500

    base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/vercel/",
   define: {
     'process.env': {} // Define empty process.env to avoid errors
   }
