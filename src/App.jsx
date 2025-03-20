3a0b05320ab4f8fa6ddb016abe063354734bd197
AREENAABDULLAH <areenaabdullah120@gmail.com>
2025-03-20 20:20:49 +0500

    doing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+
 import AppRouter from "./AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>  {/* ✅ Ensure Router is Wrapping Everything */}
+ 
       <AppRouter />
-    </BrowserRouter>
+   
   );
 }
 
